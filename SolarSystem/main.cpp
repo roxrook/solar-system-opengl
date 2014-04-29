c65758098e0fb4b7d4537d294d06a5fdc0759b2b
Chan Nguyen <atbl1511@gmail.com>
2014-04-28 23:55:07 -0700

    Initial Commit

---
+++ SolarSystem/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  SolarSystem
+//
+//  Created by chan on 4/28/14.
+//  Copyright (c) 2014 Chan Nguyen. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
